5faeffb0338757ce46eb98a02d930ebea06dc50b
Steve Gattuso <steve@stevegattuso.me>
2016-04-09 13:26:32 -0400

    Everything is configurable via the command line

--- lib/twproxy/proxy.rb
+++ lib/twproxy/proxy.rb
@@ -10,75 +10,79 @@ class TWProxy < Sinatra::Base
   #
   before do
     @accepted_token = Digest::SHA1.hexdigest(
-      "#{ENV['WIKI_USER']}#{ENV['WIKI_PASS']}#{ENV['WIKI_AUTH']}"
+      "#{settings.username}#{settings.password}#{settings.auth}"
     )
 
-    token = request.cookies['auth']
+    token = request.cookies["auth"]
 
     if token == @accepted_token
       @authenticated = true
-    elsif request.path != '/login'
-      redirect '/login'
+    elsif request.path != "/login"
+      redirect "/login"
     end
   end
 
   ##
   # Authentication handlers
   #
-  get '/login' do
-    redirect '/' if @authenticated
+  get "/login" do
+    redirect "/" if @authenticated
     haml :login
   end
 
-  post '/login' do
-    hashed_pass = Digest::SHA1.hexdigest(params['pass'])
-    user = params['user'] == ENV['WIKI_USER']
-    pass = hashed_pass == ENV['WIKI_PASS']
+  post "/login" do
+    hashed_pass = Digest::SHA1.hexdigest(params["pass"])
+    user = params["user"] == settings.username
+    pass = hashed_pass == settings.password
 
-    totp = ROTP::TOTP.new(ENV['WIKI_AUTH'])
-    auth = totp.verify(params['auth'])
+    if settings.auth
+      totp = ROTP::TOTP.new(settings.auth)
+      auth = totp.verify(params["auth"])
+    else
+      auth = true
+    end
 
     if !auth
-      @error = 'Auth code is busted'
+      @error = "Auth code is busted"
       haml :login
     elsif user && pass && auth
       token = Digest::SHA1.hexdigest(
-        "#{params['user']}#{hashed_pass}#{ENV['WIKI_AUTH']}"
+        "#{params["user"]}#{hashed_pass}#{settings.auth}"
       )
-      response.set_cookie('auth', value: token, secure: true, httponly: true)
+      response.set_cookie("auth", value: token, secure: settings.enable_ssl, httponly: true)
 
-      redirect '/'
+      redirect "/"
     else
-      @error = 'Your shit is whack'
+      @error = "Invalid username/password"
       haml :login
     end
   end
 
-  get '/logout' do
-    session[:auth] = ''
-    redirect '/login'
+  get "/logout" do
+    session[:auth] = ""
+    redirect "/login"
   end
 
   ##
   # Wiki proxy
   #
   put /\/(.*)/ do
-    uri = URI.parse("#{ENV['WIKI_URL']}#{URI::encode(params[:captures][0])}")
+    uri = URI.parse("#{settings.url}#{URI::encode(params[:captures][0])}")
     http = Net::HTTP.new(uri.host, uri.port)
 
-    req = Net::HTTP::Put.new(uri.request_uri, initheader = { 'Content-Type' => 'application/json'})
+    req = Net::HTTP::Put.new(uri.request_uri, initheader = { "Content-Type" => "application/json"})
     request.body.rewind
     req.body = request.body.read
 
     resp = http.request(req)
 
     status resp.code
-    headers({"Etag" => resp['Etag']})
+    headers({"Etag" => resp["Etag"]})
     resp.body
   end
 
   delete /\/(.*)/ do
-    uri = URI.parse("#{ENV['WIKI_URL']}#{URI::encode(params[:captures][0])}")
+    uri = URI.parse("#{settings.url}#{URI::encode(params[:captures][0])}")
     http = Net::HTTP.new(uri.host, uri.port)
 
     req = Net::HTTP::Delete.new(uri.request_uri)
@@ -89,7 +93,7 @@ class TWProxy < Sinatra::Base
   end
 
   get /\/(.*)/ do
-    uri = URI.parse("#{ENV['WIKI_URL']}#{params[:captures][0]}")
+    uri = URI.parse("#{settings.url}#{params[:captures][0]}")
     http = Net::HTTP.new(uri.host, uri.port)
     req = Net::HTTP::Get.new(uri.request_uri)
 
